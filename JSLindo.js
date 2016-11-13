c3271bb3ae212132e1c388cfe3a85435d14aef82
Lavínia Paganini <laviniapaganini@gmail.com>
2016-11-13 16:50:59 -0300

    Versão estável do círculo sendo criado

--- JSLindo.js
+++ JSLindo.js
@@ -14,17 +14,9 @@ var showPontos = true
 
 stage.on('click', function(e) {
 	var existe = false
-	for(var i =0; i < circles.length; i++){
-		if(circles[i] == e.target){
-			points[i].x = e.x
-			points[i].y = e.y
-			existe = true
-		}
-	}
 	if(!existe){
-		circles.push(new Circle(e.x, e.y, 5).attr('fillColor', 'gray').on('drag', function(evt){
-      this.attr({x: evt.x, y:evt.y});
-    }));
+		circles.push(new Circle(e.x, e.y, 5).attr('fillColor', 'gray').addTo(stage))
+		points.push(new Point(e.x, e.y))
 	}
 });
 
